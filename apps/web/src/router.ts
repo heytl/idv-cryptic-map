f65b11dad03d63d8586137594b3294c623d6dd20
heytl <heytl@qq.com>
2026-07-23 16:00:11 +0800

    fix: 路由跳转时自动重置页面全局滚动位置至顶部

--- apps/web/src/router.ts
+++ apps/web/src/router.ts
@@ -4,4 +4,10 @@ import { routes } from './routes';
 export const router = createRouter({
   history: createWebHashHistory(),
   routes,
+  scrollBehavior(_to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { top: 0, left: 0 };
+  },
 });
